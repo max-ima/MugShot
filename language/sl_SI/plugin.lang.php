465f5b47f7b88ff65f5f4651471f398282efb20e
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-05 23:37:23 +0100

    new language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['MugShot'] = 'MugShot';
+$lang['Name that face!'] = 'Poimenujte ta obraz!';
+$lang['Configuration'] = 'Konfiguracija';
+$lang['Save'] = 'Shrani';
+$lang['Groups'] = 'Skupine';
+$lang['Type in a search term'] = 'Vnesite iskalni izraz';
+$lang['The following groups can use MugShot to tag people'] = 'Naslednje skupine lahko uporabljajo MugShot za označevanje ljudi';
+$lang['MugShot to crop tagged faces from photos'] = 'MugShot za obrezovanje označenih obrazov s slik';
+$lang['ALLOW'] = 'DOVOLI';
+$lang['DO NOT ALLOW'] = 'NE DOVOLJUJTE';
+$lang['Maximum number of tags to load client side for auto complete functionality'] = 'Največje število oznak za nalaganje odjemalske strani za samodejno dokončanje funkcionalnosti';
+
+$lang['Enable cropping of faces for automated tagging'] = 'Omogoči obrezovanje obrazov za samodejno označevanje';
+$lang['This requires ImageMagick to be installed, including the PHP plugin'] = 'Za to je treba namestiti ImageMagick, vključno z PHP vtičnik';
+$lang['Save'] = 'Shrani';
